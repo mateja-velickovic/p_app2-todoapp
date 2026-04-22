bf46387dc37be59b697e7f39f1e481ecae3cb191
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2026-04-22 13:51:09 +0200

    fix(url): Modification de l'url du backend localhost > backend

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173', // your frontend
     env: {
-      BACKEND_URL: 'http://localhost:3000', // your API
+      BACKEND_URL: 'http://backend:3000', // your API
       USER_EMAIL: "john@example.com",
       USER_PASSWORD: "GRY0xa7jRRPfLy9XxC3v",
     },
