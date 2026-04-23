8e63f413cf5508dfbb087f928969918b35951e42
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2026-04-23 09:02:22 +0200

    fix(cypress): Mise à jour des ports / hosts

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173', // your frontend
+    baseUrl: 'http://web:8080', // your frontend
     env: {
-      BACKEND_URL: 'http://backend:3000', // your API
+      BACKEND_URL: 'http://api:3000', // your API
       USER_EMAIL: "john@example.com",
       USER_PASSWORD: "GRY0xa7jRRPfLy9XxC3v",
     },
