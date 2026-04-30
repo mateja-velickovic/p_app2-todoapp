84387623422e62c69d3b9d0e532997488e482439
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2026-04-30 10:09:42 +0200

    feat(sentry): Mise en place de la config de Sentry pour le frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,9 +6,16 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 import App from './App.vue';
 import router from '@/router';
+import * as Sentry from '@sentry/vue';
 
 const app = createApp(App);
 const pinia = createPinia();
+
+Sentry.init({
+  app,
+  dsn: "https://963e479f7bf5ab7e198a346e0236848c@o4511302587187200.ingest.de.sentry.io/4511302595969104",
+  sendDefaultPii: true,
+});
 
 // Use the plugin to persist the store
 pinia.use(piniaPluginPersistedstate);
